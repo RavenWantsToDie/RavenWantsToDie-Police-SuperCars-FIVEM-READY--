1d38a7c20d5df8e7d6dfc1d28653111329e0ad24
RavenWantsToDie <61123857+RavenWantsToDie@users.noreply.github.com>
2020-02-16 12:48:11 -0800

    Add files via upload

---
+++ PoliceSuperCarPack [Fivem Ready]/__resource.lua
@@ -0,0 +1,23 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+ 
+ 
+ -- Head over to https://docs.fivem.net/game-references/data-files/ for the data files
+files {
+    'carcols.meta',
+    'carvariations.meta',
+     'vehiclelayouts.meta',
+     'vehicles.meta',
+    'handling.meta',
+	'dlctext.meta',
+	'shop_vehicle.meta',
+	'zievscontentunlocks.meta',
+}
+
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
+data_file 'DLC_TEXT_FILE' 'dlctext.meta'
+data_file 'SHOP_VEHICLE_FILE' 'shop_vehicle.meta'
+data_file 'ZIEVS_CONTENT_UNLOCKS_FILE' 'zievscontentunlocks.meta'
